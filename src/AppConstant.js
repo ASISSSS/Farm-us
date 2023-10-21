be9251ba23a230b8e4e4f5019b36434a5b08552e
Ezerhart <q.juvin@laposte.net>
2023-10-21 16:50:42 +0200

    Ajout des spawners et affichage d'un dummy pour montrer une progression

--- src/AppConstant.js
+++ src/AppConstant.js
@@ -16,8 +16,27 @@ const PLAYER = {
     POSITION_START_Y: 2100,
 }
 
+const CAMERA = {
+    SPEED: 60
+}
+
+const DUMMIES_TYPES = {
+    BLUE: 0,
+    GREEN: 1,
+    YELLOW: 2
+}
+
+const SPWANERS = {
+    BLUE: { x: 4000, y: 1300, timer: 1000, type: DUMMIES_TYPES.BLUE },
+    GREEN: { x: 4600, y: 1300, timer: 2000, type: DUMMIES_TYPES.GREEN },
+    YELLOW: { x: 5200, y: 1300, timer: 100, type: DUMMIES_TYPES.YELLOW }
+}
+
 export {
     VIEW,
     WORLD_SIZE,
     PLAYER,
+    DUMMIES_TYPES,
+    SPWANERS,
+    CAMERA
 }
---
+++ src/game/entity/index.js
@@ -0,0 +1,7 @@
+import Player from './Player'
+import Spawner from './Spawner'
+
+export {
+    Player,
+    Spawner
+}
